7ccb77884978f4ede241dd08a77510278aaf5368
NayanPython <choudharyji527@gmail.com>
2026-07-27 23:47:52 +0530

    return

--- src/pages/AuthCallback.tsx
+++ src/pages/AuthCallback.tsx
@@ -26,3 +26,9 @@ export default function AuthCallback() {
     return () => { unsubscribe?.(); };
   }, [loadUser, navigate]);
 
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="w-12 h-12 border-4 border-indigo-500/30 border-t-indigo-500 rounded-full animate-spin" />
+    </div>
+  );
+}
