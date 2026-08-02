163266b4dd26255d0d9381ae5568a9d04a81deba
NayanPython <choudharyji527@gmail.com>
2026-08-02 23:44:11 +0530

    battle-01

---
+++ src/pages/BattleResultsPage.tsx
@@ -0,0 +1,7 @@
+import { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { Trophy, Crown, Swords, ArrowRight, Zap, Target, Users } from 'lucide-react';
+import { cn } from '../lib/utils';
+import { useGameStore } from '../store/gameStore';
+
